2ca840a0012cb12ba264f5d42f7e8f113fcddc63
tmaras02 <tomislav.maras2002@gmail.com>
2025-03-19 10:04:16 +0100

    feat: dodana nova metoda

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ if __name__ == "__main__":
 def nova_funkcija():
     return "Ovo je nova funkcija."
     # Ovo je moj komentar - Domagoj Butić
+
+def nova_metoda(self):
+    return "Ovo je nova metoda"
