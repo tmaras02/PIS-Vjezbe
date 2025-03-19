25fd8c439a621d31c829c9237cf3524365272aa0
dbutic <dbutic9@gmail.com>
2025-03-19 10:05:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ if __name__ == "__main__":
 def nova_funkcija():
     return "Ovo je nova funkcija."
     # Ovo je moj komentar - Domagoj Butić
+def nova_metoda():
+    return "Ovo je nova metoda 2."
